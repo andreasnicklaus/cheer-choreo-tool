fb5bd6fdd6bb52408bf661e67afc9d6c45341f99
Andreas Nicklaus <andreas_nicklaus@web.de>
2024-10-09 12:22:43 +0200

    Included Router in Matomo Tracking

--- app/src/plugins/matomo.js
+++ app/src/plugins/matomo.js
@@ -1,9 +1,11 @@
+import router from "@/router";
 import Vue from "vue";
 import VueMatomo from "vue-matomo";
 
 Vue.use(VueMatomo, {
   host: "https://matomo.andreasnicklaus.de",
   siteId: 3,
+  router,
 });
 
 window._paq.push(["trackPageView"]);
