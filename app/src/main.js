281f059d9a65127878f1a7ff608c230b1360ce3b
Andreas Nicklaus <andreas_nicklaus@web.de>
2024-10-09 12:58:40 +0200

    Moved Matomo Tracking to main.js

--- app/src/main.js
+++ app/src/main.js
@@ -3,14 +3,20 @@ import "mutationobserver-shim";
 import Vue from "vue";
 import "./plugins/bootstrap-vue";
 import "./plugins/vue-meta";
-import "./plugins/matomo";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import vueMatomo from "vue-matomo";
 
 Vue.config.productionTip = false;
+
+Vue.use(vueMatomo, {
+  host: "https://matomo.andreasnicklaus.de",
+  siteId: 3,
+  router,
+});
 
 new Vue({
   router,
--- app/src/plugins/matomo.js
+++
@@ -1,9 +0,0 @@
-import router from "@/router";
-import Vue from "vue";
-import VueMatomo from "vue-matomo";
-
-Vue.use(VueMatomo, {
-  host: "https://matomo.andreasnicklaus.de",
-  siteId: 3,
-  router,
-});
