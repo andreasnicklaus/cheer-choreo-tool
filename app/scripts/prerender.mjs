0d7c97035fb593be61d4ae14f33d6b274a9fed97
Andreas Nicklaus <andreas_nicklaus@web.de>
2026-03-25 11:55:18 +0100

    perf(CI): :zap: skips puppeteer install for build step

--- app/scripts/prerender.mjs
+++ app/scripts/prerender.mjs
@@ -246,6 +246,7 @@ async function main() {
 
   const browser = await puppeteer.launch({
     headless: "new",
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
