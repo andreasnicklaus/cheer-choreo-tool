865294224f7cbf7c9bbd4cc88ffc54bc95e26dbd
Andreas Nicklaus <andreas_nicklaus@web.de>
2026-04-06 07:59:57 +0200

    chore: :package: updates package versions and lints config files

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,42 +1,42 @@
-import globals from 'globals'
-import pluginVue from 'eslint-plugin-vue'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
   {
     ignores: [
-      '**/dist/**',
-      '**/node_modules/**',
-      '**/public/docs/**',
-      '**/coverage/**',
-      '**/dev-dist/**',
-      '**/.vite/**',
-      '**/vite.config.*timestamp*',
-      '**/*.min.js'
-    ]
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/public/docs/**",
+      "**/coverage/**",
+      "**/dev-dist/**",
+      "**/.vite/**",
+      "**/vite.config.*timestamp*",
+      "**/*.min.js",
+    ],
   },
-  ...pluginVue.configs['flat/recommended'],
+  ...pluginVue.configs["flat/recommended"],
   {
-    files: ['**/*.vue'],
+    files: ["**/*.vue"],
     languageOptions: {
       parserOptions: {
-        parser: tseslint.parser
-      }
-    }
+        parser: tseslint.parser,
+      },
+    },
   },
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   eslintPluginPrettierRecommended,
   {
     rules: {
-      'prettier/prettier': ['error', { endOfLine: 'auto' }]
-    }
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
   }
-)
+);
