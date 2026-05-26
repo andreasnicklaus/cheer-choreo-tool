d634fcaf7f78fc49c9d85f011a01f4bf6fb074ac
Andreas Nicklaus <andreas_nicklaus@web.de>
2026-05-26 23:25:42 +0200

    build(FE): :package: remove vite-tsconfig-paths dependency and update vite config for tsconfig path resolution

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { readFileSync, existsSync, writeFileSync } from "fs";
 import vue from "@vitejs/plugin-vue";
-import tsconfigPaths from "vite-tsconfig-paths";
 import Icons from "unplugin-icons/vite";
 import Components from "unplugin-vue-components/vite";
 import IconsResolve from "unplugin-icons/resolver";
@@ -47,6 +46,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {},
+    tsconfigPaths: true,
   },
   optimizeDeps: {
     include: ["vue3-html2pdf", "jspdf"],
@@ -58,7 +58,6 @@ export default defineConfig({
   plugins: [
     vue(),
     preserveManifestTrailingNewline(),
-    tsconfigPaths(),
     Components({
       resolvers: [IconsResolve()],
       dts: true,
